7386403051279a4f585fa5ceb2b672922475936b
mramey-dev <rameykenya93@gmail.com>
2026-04-16 16:44:12 -0500

    added CSS and HTML functions

--- scripts/GuestList.js
+++ scripts/GuestList.js
@@ -1,3 +1,17 @@
 import { getGuests } from "./database.js";
 
 const guests = getGuests();
+
+export const guestList = () => {
+  let guestHTML = `<section class="guests">
+  <h2>Park Visitors</h2>
+  <ul>`;
+
+  for (const guest of guests) {
+    guestHTML += `<li>${guest.firstName} ${guest.lastName}</li>`;
+  }
+  guestHTML += `</ul>
+  </section>`;
+
+  return guestHTML;
+};
--- scripts/ServicesList.js
+++ scripts/ServicesList.js
@@ -1,3 +1,34 @@
-import { getServices } from "./database.js";
+import { getServices, getParkAreaServices } from "./database.js";
 
 const services = getServices();
+
+export const servicesHTMLforPark = (parkID) => {
+  const parkAreaServices = getParkAreaServices();
+  let html = `<ul>`;
+
+  for (const relationship of parkAreaServices) {
+    if (relationship.parkAreaId === parkID) {
+      for (const service of services) {
+        if (service.id === relationship.serviceID) {
+          html += `<li>${service.name}</li>`;
+        }
+      }
+    }
+  }
+  html += "</ul>";
+  return html;
+};
+
+export const servicesBar = () => {
+  let servicesHTML = `<section class="services-bar">`;
+
+  for (const service of services) {
+    servicesHTML += `<button class="service-button">
+        <span class="icon">${service.icon}</span>
+        <span class ="label">${service.name}</span>
+    </button>`;
+  }
+
+  servicesHTML += "</section>";
+  return servicesHTML;
+};
